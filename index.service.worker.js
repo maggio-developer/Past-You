20abbc5fa3f90a25eac2ac86f967f9b227d39819
maggio-developer <maggio.mab@gmail.com>
2026-03-15 21:04:39 -0300

    final version

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1773501393|1082452544';
+const CACHE_VERSION = '1773619439|688280688';
 /** @type {string} */
 const CACHE_PREFIX = 'Past You Beta-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
